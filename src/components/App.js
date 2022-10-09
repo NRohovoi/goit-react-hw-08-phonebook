e6669a8f516e887ef32beb1533bddde0a3ea7b13
Nikita Rohovoi <NRohovoi@gmail.com>
2022-10-09 18:15:18 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -46,6 +46,20 @@ export class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     return (
